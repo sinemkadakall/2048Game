299f63e9d00a454158719aea90ae4f847519d905
sinemsooplay <sinemkadakall@gmail.com>
2024-10-16 15:09:25 +0300

    Tile states were made

---
+++ 2048/Assets/Scripts/TileState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Tile State")]
+public class TileState : ScriptableObject
+{
+    public Color backgroundColor;
+    public Color textColor;
+
+
+
+}
